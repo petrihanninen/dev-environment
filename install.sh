9cd151fa12ee8140bea507aae5785a50c54642d5
Petri Hänninen <petri.hanninen@dingle.fi>
2018-07-25 16:48:04 +0300

    Add missing recursive flag to installing of VSCode extensions

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ cp system/.profile ~/
 . ~/.profile
 
 # Install VSCode settings
-cp vscode/extensions ~/.vscode-insiders/
+cp -r vscode/extensions ~/.vscode-insiders/
 cp vscode/keybindings.json ~/Library/Application\ Support/Code\ -\ Insiders/User/
 cp vscode/settings.json ~/Library/Application\ Support/Code\ -\ Insiders/User/
