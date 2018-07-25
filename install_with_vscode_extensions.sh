10946ab0f2c46065e16bf3fe0b496a886458c56e
Petri Hänninen <petri.hanninen@dingle.fi>
2018-07-25 17:56:55 +0300

    Add separate scripts for pulling and installing with vscode extensions

--- install_with_vscode_extensions.sh
+++ install_with_vscode_extensions.sh
@@ -5,6 +5,7 @@ cp system/.profile ~/
 . ~/.profile
 
 # Install VSCode settings
+cp -r vscode/extensions ~/.vscode-insiders/
 cp vscode/keybindings.json ~/Library/Application\ Support/Code\ -\ Insiders/User/
 cp vscode/settings.json ~/Library/Application\ Support/Code\ -\ Insiders/User/
 
--- pull_with_vscode_extensions.sh
+++ pull_with_vscode_extensions.sh
@@ -4,6 +4,7 @@
 cp ~/.profile system/
 
 # VSCode
+rm -rf vscode/extensions && cp -r ~/.vscode-insiders/extensions vscode/
 cp ~/Library/Application\ Support/Code\ -\ Insiders/User/keybindings.json vscode/
 cp ~/Library/Application\ Support/Code\ -\ Insiders/User/settings.json vscode/
 
