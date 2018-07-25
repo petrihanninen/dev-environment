48bcfdbf8a5d14305288f3ba284d6e3684bc04c3
Petri Hänninen <petri.hanninen@dingle.fi>
2018-07-25 16:31:22 +0300

    Add VSCode to run script

--- run.sh
+++ run.sh
@@ -3,3 +3,8 @@
 # Install and reload .profile
 cp system/.profile ~/
 . ~/.profile
+
+# Install VSCode settings
+cp vscode/extensions ~/.vscode-insiders/
+cp vscode/keybindings.json ~/Library/Application\ Support/Code\ -\ Insiders/User/
+cp vscode/settings.json ~/Library/Application\ Support/Code\ -\ Insiders/User/
