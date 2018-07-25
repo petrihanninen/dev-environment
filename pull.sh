470039a80937b71a42dce30cf518d34487856c82
Petri Hänninen <petri.hanninen@dingle.fi>
2018-07-25 16:50:09 +0300

    Add pull script for updating repo from system settings

---
+++ pull.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Bash .profile
+cp ~/.profile system/
+
+# VSCode
+rm -rf vscode/extensions && cp -r ~/.vscode-insiders/extensions vscode/
+cp ~/Library/Application\ Support/Code\ -\ Insiders/User/keybindings.json vscode/
+cp ~/Library/Application\ Support/Code\ -\ Insiders/User/settings.json vscode/
